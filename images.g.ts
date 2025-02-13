7a328998991bd70e3769656f2b25cfcad0d2d53e
jacksonjbd <55103892+jacksonjbd@users.noreply.github.com>
2025-02-12 22:17:46 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image4":
+            case "Player3":return img`
+. . . . . . . . . 2 d 2 . . . . 
+. . . d d d d d . d 7 d . . . . 
+. . d 1 1 1 1 1 d d 7 d . . . . 
+. d 1 1 1 5 1 1 1 d 7 d . . . . 
+. d 1 1 1 5 5 1 2 d 7 d . . . . 
+. d 1 1 2 2 5 5 d d 7 d . . . . 
+. d 1 1 1 3 7 d 5 d 7 d . . . . 
+. d 2 1 1 3 7 d 1 d 7 d . . . . 
+. . d 2 1 1 1 1 2 d 7 d . . . . 
+. . d 1 2 2 2 2 d d 7 d . . . . 
+. . d 2 1 1 2 1 1 d 7 d . . . . 
+. . . d 2 2 1 1 2 d 7 d . . . . 
+. . . d 2 2 2 2 d d 7 d . . . . 
+. 2 d 2 2 2 2 1 2 d 7 d . . . . 
+. 2 d 2 1 2 d d d d 7 d . . . . 
+. 2 d d d d 2 2 2 2 d 2 . . . . 
+`;
+            case "image3":
+            case "Player0":return img`
+. . . . 2 d 2 . . . . . . . . . 
+. . . . d 7 d . d d d d d . . . 
+. . . . d 7 d d 6 6 6 6 6 d . . 
+. . . . d 7 d 6 6 6 5 6 6 6 d . 
+. . . . d 7 d 2 6 5 5 6 6 6 d . 
+. . . . d 7 d d 5 5 2 2 6 6 d . 
+. . . . d 7 d 5 d 7 3 6 6 6 d . 
+. . . . d 7 d 6 d 7 3 6 6 2 d . 
+. . . . d 7 d 2 6 6 6 6 2 d . . 
+. . . . d 7 d d 2 2 2 2 6 d . . 
+. . . . d 7 d 6 6 2 6 6 2 d . . 
+. . . . d 7 d 2 6 6 2 2 d . . . 
+. . . . d 7 d d 2 2 2 2 2 d . . 
+. . . . d 7 d d 2 2 2 6 2 d 2 . 
+. . . . d 7 d 2 6 2 d d d d 2 . 
+. . . . 2 d 2 d d d 2 2 2 2 2 . 
+`;
+            case "image1":
+            case "Player1":return img`
+. . . . . . . . . 2 d 2 . . . . 
+. . . d d d d d d d 7 d . . . . 
+. . d 1 1 1 1 1 2 d 7 d . . . . 
+. d 1 1 1 5 1 1 1 d 7 d . . . . 
+. d 1 1 1 5 5 1 2 d 7 d . . . . 
+. d 1 1 2 2 5 5 d d 7 d . . . . 
+. d 1 1 1 3 7 d 5 d 7 d . . . . 
+. d 2 1 1 3 7 d 1 d 7 d . . . . 
+. . d 2 1 1 1 1 2 d 7 d . . . . 
+. . d 1 2 2 2 2 d d 7 d . . . . 
+. . d 2 1 1 2 1 1 d 7 d . . . . 
+. . . d 2 2 1 1 2 d 7 d . . . . 
+. . d 1 1 2 2 2 d d 7 d . . . . 
+. 2 d 1 2 d 1 2 d d 7 d . . . . 
+. 2 d d d d 1 1 2 d 7 d . . . . 
+. 2 2 2 2 2 d d d 2 d 2 . . . . 
+`;
+            case "image2":
+            case "Player2":return img`
+. . . . 2 d 2 . . . . . . . . . 
+. . . . d 7 d d d d d d d . . . 
+. . . . d 7 d 2 6 6 6 6 6 d . . 
+. . . . d 7 d 6 6 6 5 6 6 6 d . 
+. . . . d 7 d 2 6 5 5 6 6 6 d . 
+. . . . d 7 d d 5 5 2 2 6 6 d . 
+. . . . d 7 d 5 d 7 3 6 6 6 d . 
+. . . . d 7 d 6 d 7 3 6 6 2 d . 
+. . . . d 7 d 2 6 6 6 6 2 d . . 
+. . . . d 7 d d 2 2 2 2 6 d . . 
+. . . . d 7 d 6 6 2 6 6 2 d . . 
+. . . . d 7 d 2 6 6 2 2 d . . . 
+. . . . d 7 d d 2 2 2 6 6 d . . 
+. . . . d 7 d d 2 6 d 2 6 d 2 . 
+. . . . d 7 d 2 6 6 d d d d 2 . 
+. . . . 2 d 2 d d d 2 2 2 2 2 . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
